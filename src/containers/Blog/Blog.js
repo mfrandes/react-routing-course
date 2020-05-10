3f3b8dcab10e6d2be209d43f3e167582b00a2c69
Mircea <frandesmyrce@gmail.com>
2020-05-10 17:55:52 +0300

    set up links (nav)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,9 +14,9 @@ class Blog extends Component {
         error: false
     }
 
-    componentDidMount () {
-        axios.get( '/posts' )
-            .then( response => {
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
                     return {
@@ -24,33 +24,45 @@ class Blog extends Component {
                         author: 'Max'
                     }
                 });
-                this.setState({posts: updatedPosts});
+                this.setState({ posts: updatedPosts });
                 // console.log( response );
-            } )
+            })
             .catch(error => {
                 // console.log(error);
-                this.setState({error: true});
+                this.setState({ error: true });
             });
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        this.setState({ selectedPostId: id });
     }
 
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
+                return <Post
+                    key={post.id}
+                    title={post.title}
                     author={post.author}
                     clicked={() => this.postSelectedHandler(post.id)} />;
             });
         }
 
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="/">Home</a>
+                            </li>
+                            <li>
+                                <a href="/new-post">New Post</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
